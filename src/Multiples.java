e1e50829fe1ad4dce989092701e0b81d5c9310c0
astridhu123 <astreedhu@gmail.com>
2024-09-09 16:29:58 -0400

    task 4

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+    //write a main method that **prints** how many multiples of 3 or 5 there are below 1000.
+    public static void main(String[] args) {
+        int count = 0;
+
+        for (int i = 1; i < 1000; i++) {
+            boolean multipleOf3 = (i % 3 == 0);
+            boolean multipleOf5 = (i % 5 == 0);
+
+            if (multipleOf3 || multipleOf5) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
+
+
+
+
+
